506d96ec86d449a0894fc18415202a38f103f79a
george hunt <georgejhunt@gmail.com>
2012-10-15 06:45:12 -0400

    bashrc, acpower mon, collapsing menu for internet, mem report

--- cfg/html/top/en/pick_services.php
+++ cfg/html/top/en/pick_services.php
@@ -8,9 +8,9 @@
 <script language="JavaScript" type="text/JavaScript">
 
 function toggleMenu(objID) {
-if (!document.getElementById) return;
-var ob = document.getElementById(objID).style;
-ob.display = (ob.display == 'block')?'none': 'block';
+	if (!document.getElementById) return;
+	var ob = document.getElementById(objID).style;
+	ob.display = (ob.display == 'block')?'none': 'block';
 }
 </script>
 
@@ -19,18 +19,19 @@ ob.display = (ob.display == 'block')?'none': 'block';
 <body>
 <div class="centerframe">
 <h2>Internet:</h2>
-  <input name="have_internet" type="checkbox" value="" />
+  <input name="have_internet" class="mH" onclick="toggleMenu('menu1')"type="checkbox" value="" />
 Check here if you have internet service and want the School Server to provide internet services to XO laptops.<br />
-  <input name="name_server_censor" type="checkbox" class="level1" value="" />
+<div id="menu1" class="mL">
+  <input name="name_server_censor" class="mO" type="checkbox"  value="" />
 Enable Domain Name Service content controls using OPENDNS (is mainteined for schools worldwide by specialists)<br />
-  <input name="dans_guardian_censor" type="checkbox" class="level1" value="" />
+  <input name="dans_guardian_censor" class="mO" type="checkbox"  value="" />
 Install Dan's Guardian content filter (permits local control, requires setup, continued maintenance) 
 <h3>Internet Speedups</h3>
 Is your internet speed faster than 1Megabit for 5 students, 5 Megabits for 30 students, or 15Mebagits for 75 students? If not:<br />
-<div class="level1">  
-<input name="enable_squid" type="checkbox" value="" />
+  
+<input name="enable_squid" class="mO" type="checkbox" value="" />
 Enable local web page storage for later fast access<br />
-  <input name="enable_named" type="checkbox" value="" />
+  <input name="enable_named" class="mO" type="checkbox" value="" />
 Enable local Domain Name storage for faster access</div>
 </div>
 </body>
--- cfg/html/top/en/xo_services.php
+++ cfg/html/top/en/xo_services.php
@@ -26,9 +26,9 @@
 <script language="JavaScript" type="text/JavaScript">
 
 function toggleMenu(objID) {
-if (!document.getElementById) return;
-var ob = document.getElementById(objID).style;
-ob.display = (ob.display == 'block')?'none': 'block';
+	if (!document.getElementById) return;
+	var ob = document.getElementById(objID).style;
+	ob.display = (ob.display == 'block')?'none': 'block';
 }
 </script>
 
