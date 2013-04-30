b9492177c8bd5b784eb5ac10bb108c78b36c1757
Jerry <jvonau@shaw.ca>
2013-04-30 01:58:17 -0500

    oops broke squid

--- plugins.d/squid/squid.sh
+++ plugins.d/squid/squid.sh
@@ -13,18 +13,18 @@ function squid()
             etckeeper-if-selected 'xs-setup force-create httpcache flag'
         fi
         /etc/sysconfig/iptables-config
-        systemctl condrestart iptables.service
+        systemctl restart iptables.service
         systemctl enable squid.service 2>&1 | tee -a $LOG
-        set +e; systemctl condrestart squid.service 2>&1 | tee -a $LOG; set -e
+        set +e; systemctl restart squid.service 2>&1 | tee -a $LOG; set -e
         ;;
     "no")
         systemctl disable squid.service 2>&1 | tee -a $LOG
         systemctl stop squid.service 2>&1 | tee -a $LOG
         rm /etc/sysconfig/xs_httpcache_on
         rm $SETUPSTATEDIR/squid
-        /etc/sysconfig/iptables-config
         # reinitialize the iptables to just use masqueradeing
-        systemctl condrestart iptables.service
+        /etc/sysconfig/iptables-config
+        systemctl restart iptables.service
         ;;
     esac
 }
