b0b05b7ab71799818f03c1e2ed1330e84e505cbc
Jerry <jvonau@shaw.ca>
2013-05-01 19:11:06 -0500

    remove quotes from symlink creation

--- plugins.d/httpd/httpd.sh
+++ plugins.d/httpd/httpd.sh
@@ -31,9 +31,9 @@ function httpd()
         # if httpd version is 2.4.4, use new syntax for access control
         ### saw an error around this area while installing symlink not present 
         if [ $(rpm -q httpd | grep fc17) ]; then
-            ln -fs "$CFGDIR/etc/httpd/conf.d/xs-2.2.conf /etc/httpd/conf.d/xs-2.2.conf" 2>&1 | tee -a $LOG
+            ln -fs $CFGDIR/etc/httpd/conf.d/xs-2.2.conf /etc/httpd/conf.d/xs-2.2.conf | tee -a $LOG
         else
-            ln -fs "$CFGDIR/etc/httpd/conf.d/xs-2.4.conf /etc/httpd/conf.d/xs-2.4.conf" 2>&1 | tee -a $LOG
+            ln -fs $CFGDIR/etc/httpd/conf.d/xs-2.4.conf /etc/httpd/conf.d/xs-2.4.conf | tee -a $LOG
         fi
 
 		etckeeper-if-selected "modified /etc/httpd/conf/httpd-xs.conf"
