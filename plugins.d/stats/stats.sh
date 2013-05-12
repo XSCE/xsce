118d999437a938a075ba227929446413bb552f92
george hunt <georgejhunt@gmail.com>
2013-05-12 14:53:29 -0400

    put the config file in a stats folder

--- plugins.d/stats/stats.sh
+++ plugins.d/stats/stats.sh
@@ -3,6 +3,7 @@ function stats {
 	"yes")
         mkdir -p /library/sugar-stats/rrd
         mkdir -p /library/sugar-stats/log
+        cp /etc/stats/sugar-stats.conf.in /etc/stats/sugar-stats.conf
         if [ ! -e /home/admin/openssl/server.key c]; then
             openssl genrsa -des3 -out /home/admin/openssl/server.key 1024
             cp /home/admin/openssl/server.key /home/admin/openssl/server.key.org
