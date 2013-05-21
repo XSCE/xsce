7388b266eda4a5e3208c489b89c629e747b85036
Jerry <jvonau@shaw.ca>
2013-05-21 15:50:32 -0500

    use yum variables for dansguardian

--- plugins.d/dansguardian/dansguardian.sh
+++ plugins.d/dansguardian/dansguardian.sh
@@ -2,7 +2,7 @@ function dansguardian()
 {
 	case "$1" in
     "yes")
-        yum -y install  dansguardian 2>&1 | tee -a $LOG
+        $YUM_CMD  dansguardian 2>&1 | tee -a $LOG
         if [ $? -ne 0 ] ; then
             echo "\n\nYum returned an error\n\n" | tee -a $LOG
             exit $YUMERROR
