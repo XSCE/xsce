162742ebe1868a933bc8325272b0335cc202c3b5
Tim Moody <timmoody@sympatico.ca>
2013-05-21 14:49:51 -0400

    Santi's config of dansguardian.sh

--- plugins.d/dansguardian/dansguardian.sh
+++ plugins.d/dansguardian/dansguardian.sh
@@ -9,6 +9,18 @@ function dansguardian()
         fi
         touch $SETUPSTATEDIR/dansguardian
         #execute the setup script
+        #Update squid conf
+        #save backup
+        cp /etc/squid/squid-xs.conf /etc/squid-xs.conf.bkp.`date +%s`
+        sed -i 's/http_port 0.0.0.0:3128 transparent/http_port 0.0.0.0:3130/g' /etc/squid/squid-xs.conf
+        sed -i 's/#acl localhost/acl localhost/' /etc/squid/squid-xs.conf
+        systemctl restart squid.service
+        
+        #Update dansguardian conf
+        cp /etc/dansguardian/dansguardian.conf /etc/dansguardian/dansguardian.conf.bkp.`date +%s`
+        sed -i 's/filterport = 8080/filterport = 3128/' /etc/dansguardian/dansguardian.conf
+        sed -i 's/proxyport = 3128/proxyport = 3130/' /etc/dansguardian/dansguardian.conf
+        systemctl restart dansguardian.service        
         ;;
     "no")
         rm $SETUPSTATEDIR/dansguardian
