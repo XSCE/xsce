bfe47c033ab9df9fe326d3f34e75856630e85b97
Jerry <jvonau@shaw.ca>
2013-05-24 11:41:40 -0500

    final fix of create-usb-repo2

--- plugins.d/startup/startup.sh
+++ plugins.d/startup/startup.sh
@@ -97,38 +97,29 @@ EOF
 
 function create-usb-repo2()
 {
+    echo "starting create-usb-repo2" | tee -a $LOG
     ARCH=`ls /var/cache/yum`
     RELEASEVER=`ls /var/cache/yum/$ARCH`
-    echo "starting create-usb-repo2" | tee -a $LOG
-    u_mnt=$(`mount | grep var/cache/yum`) | tee -a $LOG
-    if ! [ -z $u_mnt ] | tee -a $LOG
-    then
+    u_mnt="`mount | grep var/cache/yum | gawk '{print $1}'`"
+    echo "VAR-u_mnt is $u_mnt"
+    if ! [ -z $u_mnt ]; then
 	umount /var/cache/yum | tee -a $LOG
-	usbkey=""
-	for parts in `ls /dev/sd*1`
-	do
-	    if [ x$parts != 'x' ] | tee -a $LOG
-	    then
-		maybe=`ls -la /sys/class/block/ | grep usb | gawk '{print 9}'` | tee -a $LOG
-		if ! [ -z "$maybe" ] | tee -a $LOG
-		then
-		    usbkey=`findmnt -n -o TARGET -S $parts` | tee -a $LOG
-		    if [ -d $usbkey/xs-repo -a ! -d $usbkey/library ] | tee -a $LOG
-		    then
-			mkdir -p $usbkey/xs-repo/$ARCH/$RELEASEVER/local | tee -a $LOG
-			yumdownloader --destdir=$usbkey/xs-repo/$ARCH/$RELEASEVER/local xs-config-xo | tee -a $LOG
-			createrepo $usbkey/xs-repo/$ARCH/$RELEASEVER | tee -a $LOG
-			sleep 2
-			sync
-			umount $usbkey | tee -a $LOG
-		    fi
-		fi
-	    else
-		"No external media found" | tee -a $LOG
+	if [ x$u_mnt != 'x' ]; then
+	    usbkey=$(findmnt -n -o TARGET -S $u_mnt)
+	    echo "found $usbkey"
+	    if [ -d $usbkey/xs-repo -a ! -d $usbkey/library ]; then
+		mkdir -p $usbkey/xs-repo/$ARCH/$RELEASEVER/local | tee -a $LOG
+		yumdownloader --destdir=$usbkey/xs-repo/$ARCH/$RELEASEVER/local xs-config* | tee -a $LOG
+		createrepo $usbkey/xs-repo/$ARCH/$RELEASEVER | tee -a $LOG
+		sleep 2
+		sync
+		umount $usbkey | tee -a $LOG
 	    fi
-	done
+	else
+	    echo "INFO No external media found" | tee -a $LOG
+	fi
     else
-	echo "/var/cache/yum not mounted" | tee -a $LOG
+	echo "INFO /var/cache/yum is not mounted" | tee -a $LOG
     fi
     echo "leaving create-usb-repo2" | tee -a $LOG
 }
