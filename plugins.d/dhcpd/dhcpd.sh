9484123b43935626a4eb8224d737e201a9c4a5e5
george hunt <georgejhunt@gmail.com>
2013-05-25 12:25:46 -0400

    dhcpd failed to start, missing leases file

--- plugins.d/dhcpd/dhcpd.sh
+++ plugins.d/dhcpd/dhcpd.sh
@@ -8,6 +8,9 @@ function dhcpd()
             exit $YUMERROR
         fi
         touch $SETUPSTATEDIR/dhcpd
+        # fc18 (olpc 13.2.0) failed to start because leases file didn't exist
+        touch /var/lib/dhcpd/dhcpd.leases
+        chown dhcpd:dhcpd /var/lib/dhcpd/dhcpd.leases
         systemctl enable dhcpd.service 2>&1 | tee -a $LOG
         systemctl start dhcpd.service 2>&1 | tee -a $LOG
         ;;
