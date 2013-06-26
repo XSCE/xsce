3ae56351da4c26b7c97cc80127c7680dc7422876
Santiago Rodriguez <scollazo@activitycentral.com>
2013-06-26 10:16:30 +0100

    dansguardian: plugin modified to fit xsce development style

--- plugins.d/dansguardian/dansguardian.sh
+++ plugins.d/dansguardian/dansguardian.sh
@@ -1,6 +1,6 @@
 function dansguardian()
 {
-	case "$1" in
+        case "$1" in
     "yes")
         $YUM_CMD  dansguardian 2>&1 | tee -a $LOG
         if [ $? -ne 0 ] ; then
@@ -16,17 +16,19 @@ function dansguardian()
         cp /etc/squid/squid-xs.conf /etc/squid-xs.conf.bkp.`date +%s`
         sed -i 's/http_port 0.0.0.0:3128 transparent/http_port 0.0.0.0:3130/g' /etc/squid/squid-xs.conf
         sed -i 's/#acl localhost/acl localhost/' /etc/squid/squid-xs.conf
-        systemctl start squid.service
 
         #Update dansguardian conf
-        cp /etc/dansguardian/dansguardian.conf /etc/dansguardian/dansguardian.conf.bkp.`date +%s`
-        sed -i 's/filterport = 8080/filterport = 3128/' /etc/dansguardian/dansguardian.conf
-        sed -i 's/proxyport = 3128/proxyport = 3130/' /etc/dansguardian/dansguardian.conf        
-        systemctl enable dansguardian.service
-        systemctl restart dansguardian.service        
+        cp /etc/dansguardian/dansguardian.conf.in /etc/dansguardian/dansguardian.conf
+        
+	systemctl enable dansguardian.service
+        systemctl start dansguardian.service        
+
         ;;
     "no")
+        systemctl stop dansguardian
+        systemctl disable dansguardian
         rm $SETUPSTATEDIR/dansguardian
+
         ;;
     esac
 }
