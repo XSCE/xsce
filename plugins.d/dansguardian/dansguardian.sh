bddf90d047dbd08450f1919ca1e53bbd760f0c1d
Santiago Rodriguez <scollazo@activitycentral.com>
2013-06-28 01:40:44 +0100

    dansguardian: check for squid presence 	      use /library/dansguardian as cache, instead of /tmp

--- plugins.d/dansguardian/dansguardian.sh
+++ plugins.d/dansguardian/dansguardian.sh
@@ -12,14 +12,21 @@ function dansguardian()
         #execute the setup script
         #Update squid conf
         #save backup
+        if ! [ -f /etc/sysconfig/xs_httpcache_on ]
+		then
+		echo "Squid must be enabled in order to run dansguardian!"
+		return 
+	fi
         systemctl stop squid.service
-        cp /etc/squid/squid-xs.conf /etc/squid-xs.conf.bkp.`date +%s`
         sed -i 's/http_port 0.0.0.0:3128 transparent/http_port 0.0.0.0:3130/g' /etc/squid/squid-xs.conf
         sed -i 's/#acl localhost/acl localhost/' /etc/squid/squid-xs.conf
-
+	systemctl start squid.service
         #Update dansguardian conf
         cp /etc/dansguardian/dansguardian.conf.in /etc/dansguardian/dansguardian.conf
-        
+	
+	#Create directories
+	mkdir -p /library/dansguardian
+	chown dansguardian:dansguardian /library/dansguardian -R        
 	systemctl enable dansguardian.service
         systemctl start dansguardian.service        
 
