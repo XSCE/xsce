3bc1e5f314747d8a13cea2974e0f5263728932c9
Jerry Vonau <jerry@laptop.org.au>
2013-07-10 22:35:53 +0000

    modifiy IIAB.sh

---
+++ plugins.d/IIAB/IIAB.sh
@@ -0,0 +1,14 @@
+function vnc()
+{
+	case "$1" in
+	"yes")
+        $YUM_CMD Internet-in-a-Box 2>&1 | tee -a $LOG
+        if [ $? -ne 0 ] ; then
+            echo "\n\nYum returned an error\n\n" | tee -a $LOG
+            exit $YUMERROR
+        fi
+        touch $SETUPSTATEDIR/IIAB
+        ;;
+	esac
+}
+
