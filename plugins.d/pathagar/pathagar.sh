3412f4d96b0968d4734343d0821ea949e26a73d2
george hunt <georgejhunt@gmail.com>
2013-07-12 10:51:12 -0400

    pathagar bash script fixes

--- plugins.d/pathagar/pathagar.sh
+++ plugins.d/pathagar/pathagar.sh
@@ -1,12 +1,22 @@
+PATHAGARUSER=pathagar
+PATHPASSWORD="revreskoob"  # bookserver spelled backwards
 function pathagar()
 {
 	case "$1" in
 	"yes")
+      $YUM_CMD Django django-tagging mod_wsgi pathagar python-setuptools \
+                      sqlite psycopg2
         #httpd yes #-- currently on by default
        touch $SETUPSTATEDIR/pathagar
+        # make a non privileged user
+        if [ ! `grep $PATHAGARUSER /etc/passwd` ]; then
+            adduser $PATHAGARUSER
+            echo "$PATHPASSWORD" | passwd $PATHAGARUSER --stdin
+            chmod 770 /home/$PATHAGARUSER
+        fi
         cp /etc/pathagar/settings.py.in /etc/pathagar/settings.py
-        sed -i -e "s/\@\@PASSWORD\@\@/$DEFAULTPASSWORD/" /etc/pathagar/settings.py
-        sed -i -e "s/\@\@USER\@\@/$DEFAULTUSER/" /etc/pathagar/settings.py
+        sed -i -e "s/\@\@PASSWORD\@\@/$PATHPASSWORD/" /etc/pathagar/settings.py
+        sed -i -e "s/\@\@USER\@\@/$PATHAGARUSER/" /etc/pathagar/settings.py
 
         # get the python preample for site-packages where pathagar is loaded
         SITE=`python -c "import site;print(site.getsitepackages()[0])"`
@@ -18,15 +28,20 @@ function pathagar()
         LOADED=`su - postgres -c "psql -l" | gawk '{if($1=="books") print $1}'`
         if [ "$LOADED" != "books" ]; then
             # create administrative postgresql user and database for pathagar
-            su postgres -c "psql  -c 'create database books'"
-            su postgres -c "psql  -c 'create user $DEFAULTUSER;'"
-            su postgres -c "psql  -c 'create database $DEFAULTUSER;'"
-            su postgres -c "psql  -c 'grant all privileges on database books to $DEFAULTUSER;'"
+            su - postgres -c "psql  -c 'create database books'"
+            su - postgres -c "psql  -c 'create user $PATHAGARUSER;'"
+            su - postgres -c "psql  -c 'create database $PATHAGARUSER;'"
+            su - postgres -c "psql  -c 'grant all privileges on database books to $PATHAGARUSER;'"
             # moodle may already have created  apache user
             #su postgres -c "psql  -c \"create user apache;\""
-            su postgres -c "psql  -c 'create database apache;'"
-            su postgres -c "psql  -c 'grant all privileges on database books to apache;'"
+            su - postgres -c "psql  -c 'create database apache;'"
+            su - postgres -c "psql  -c 'grant all privileges on database books to apache;'"
         fi
+        sed -i -e 's/^local.*/local    all    all    trust/' \
+                /library/pgsql-xs/pg_hba.conf
+        sed -i -e 's/^host.*127.*/host     all    all    127.0.0.1\/32   trust/'\
+                /library/pgsql-xs/pg_hba.conf
+        systemctl restart postgresql-xs.service
 
         pushd $SITE/pathagar
             # replace the default development settings with our configured ones
@@ -36,10 +51,16 @@ function pathagar()
             # link our settings to project folder
             ln -s /etc/pathagar/settings.py .
             export "DJANGO_SETTINGS_MODULE=pathagar.settings"
-            echo "from django.contrib.auth.models import User; \
-                User.objects.create_superuser('admin', 'admin@schoolserver.local', '12admin')" | \
-                su - $DEFAULTUSER -c "$SITE/pathagar/manage.py shell"
-            su - $DEFAULTUSER -c "django-admin syncdb --noinput --traceback --settings=pathagar.settings"
+
+            # create a Django admin user -- first create a command string
+            CMD="from django.contrib.auth.models import User; \
+                User.objects.create_superuser\
+                ('$PATHAGARUSER', '$PATHAGARUSER@schoolserver.local',\
+                 '$PATHPASSWORD')"
+                echo "$CMD" | su - "$PATHAGARUSER" -c "$SITE/pathagar/manage.py shell"
+
+            su - $PATHAGARUSER -c "django-admin syncdb --noinput --traceback \
+                --settings=pathagar.settings"
         popd
 
         # apache needs to add books
