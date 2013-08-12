115a7ede6d3d92850f47e3a2a9c8942ff0858405
Jerry <jvonau@shaw.ca>
2013-08-12 11:06:45 -0500

    d/l only if online, guard against double mounted yum cache

--- plugins.d/startup/startup.sh
+++ plugins.d/startup/startup.sh
@@ -118,18 +118,25 @@ function create-usb-repo2()
     u_mnt=`mount | grep var/cache/yum | gawk '{print $1}' | gawk '{print $1}'`
     echo "VAR-u_mnt is $u_mnt"
     if ! [ -z $u_mnt ]; then
-        RELEASEVER=`ls /var/cache/yum/$YUM_ARCH`
-	umount /var/cache/yum | tee -a $LOG
+	RELEASEVER=`ls /var/cache/yum/$YUM_ARCH`
+	umnt_yum=`mount | grep cache/yum | sed '{print $3}'`
+	echo "yum mount points are $umnt_yum "  
+	for mnt in $umnt_yum; do
+	    echo "unmounting $mnt" | tee -a $LOG
+	    umount /var/cache/yum | tee -a $LOG
+	done
 	if [ x$u_mnt != x ]; then
 	    usbkey=$(findmnt -n -o TARGET -S $u_mnt)
 	    echo "found $usbkey"
 	    if [ -d $usbkey/xs-repo -a ! -d $usbkey/library ]; then
-		mkdir -p $usbkey/xs-repo/$YUM_ARCH/$RELEASEVER/local | tee -a $LOG
-		yumdownloader --destdir=$usbkey/xs-repo/$YUM_ARCH/$RELEASEVER/local xs-config* | tee -a $LOG
-		createrepo $usbkey/xs-repo/$YUM_ARCH/$RELEASEVER | tee -a $LOG
-		sleep 2
-		sync
-		umount $usbkey | tee -a $LOG
+		if [ $HAVE_GATEWAY = 1 ]; then 
+  		    mkdir -p $usbkey/xs-repo/$YUM_ARCH/$RELEASEVER/local | tee -a $LOG
+		    yumdownloader --destdir=$usbkey/xs-repo/$YUM_ARCH/$RELEASEVER/local xs-config* | tee -a $LOG
+		    createrepo $usbkey/xs-repo/$YUM_ARCH/$RELEASEVER | tee -a $LOG
+		    sleep 2
+		    sync
+		fi
+		umounting $usbkey | tee -a $LOG
 	    fi
 	else
 	    echo "INFO No external media found" | tee -a $LOG
