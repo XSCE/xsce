ea500325f4f01813721824da717010884dab6bbc
Jerry <jvonau@shaw.ca>
2013-08-20 20:58:34 -0500

    make printing optional

--- plugins.d/cups/cups.sh
+++ plugins.d/cups/cups.sh
@@ -1,8 +1,13 @@
 function cups()
-# this is webdav, renamed, because sugar-panel used the webdav name at httpd root
 {
 	case "$1" in
 	"yes")
+        $YUM_CMD a2ps cups cups-pk-helper ghostscript gutenprint-foomatic hpijs mpage bluez-cups \
+        cups-pdf enscript gutenprint hal-cups-utils hplip system-config-printer 2>&1 | tee -a $LOG
+        if [ $? -ne 0 ] ; then
+            echo "\n\nYum returned an error\n\n" | tee -a $LOG
+            exit $YUMERROR
+        fi
         touch $SETUPSTATEDIR/cups
         ;;
 	"no")
