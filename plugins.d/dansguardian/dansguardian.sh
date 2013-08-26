e2efc994d82c7904f6bf466d2ddba01791f66039
Jerry <jvonau@shaw.ca>
2013-08-26 03:15:14 -0500

    replace sed commands with a real conf.in file

--- plugins.d/dansguardian/dansguardian.sh
+++ plugins.d/dansguardian/dansguardian.sh
@@ -7,29 +7,27 @@ function dansguardian()
             echo "\n\nYum returned an error\n\n" | tee -a $LOG
             exit $YUMERROR
         fi
-        touch $SETUPSTATEDIR/dansguardian
-
-        #execute the setup script
-        #Update squid conf
-        #save backup
         if ! [ -f /etc/sysconfig/xs_httpcache_on ]
 		then
 		echo "Squid must be enabled in order to run dansguardian!"
 		return 
 	fi
-        systemctl stop squid.service
-        sed -i 's/http_port 0.0.0.0:3128 transparent/http_port 0.0.0.0:3130/g' /etc/squid/squid-xs.conf
-        sed -i 's/^#acl localhost/acl localhost/' /etc/squid/squid-xs.conf
-        #Update dansguardian conf
-        cp /etc/dansguardian/dansguardian.conf.in /etc/dansguardian/dansguardian.conf
-	
+
 	#Create directories
 	mkdir -p /library/dansguardian
 	chown dansguardian:dansguardian /library/dansguardian -R        
-	systemctl enable dansguardian.service
-        systemctl start dansguardian.service        
-        #Squid must be restarted after dansguardian, because dansguardian is the upstream proxy	
-	systemctl start squid.service
+
+	#Squid must be restarted config file changed	
+	systemctl stop squid.service 2>&1 | tee -a $LOG
+	cp /etc/sysconfig/squid-dg.in /etc/sysconfig/squid         
+	systemctl start squid.service 2>&1 | tee -a $LOG
+
+	#Update dansguardian conf
+	cp /etc/dansguardian/dansguardian.conf.in /etc/dansguardian/dansguardian.conf
+	systemctl enable dansguardian.service 2>&1 | tee -a $LOG
+	systemctl restart dansguardian.service 2>&1 | tee -a $LOG
+
+        touch $SETUPSTATEDIR/dansguardian
         ;;
     "no")
 	systemctl stop squid
