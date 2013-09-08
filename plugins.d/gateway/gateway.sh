7e9175bcfcf1145ba3895f78fb43b297388f649d
Jerry <jvonau@shaw.ca>
2013-09-08 16:01:39 -0500

    ensure iptables-services is installed, better logging for gatway.sh, revert to calling iptables-config to save the rule set

--- plugins.d/gateway/gateway.sh
+++ plugins.d/gateway/gateway.sh
@@ -2,6 +2,7 @@ function gateway()
 {
 	case "$1" in
 	"yes")
+        $YUM_CMD iptables iptables-services 2>&1 | tee -a $LOG 
         cp /etc/sysconfig/olpc-scripts/iptables-xs.in /etc/sysconfig/olpc-scripts/iptables-xs
         cp /etc/sysconfig/olpc-scripts/ip6tables-xs.in /etc/sysconfig/olpc-scripts/ip6tables-xs
         cp /etc/sysconfig/iptables-config.in /etc/sysconfig/iptables-config
@@ -10,18 +11,18 @@ function gateway()
         cp  $CFGDIR/etc/systemd/system/iptables.service $DESTDIR/etc/systemd/system
 
         set +e
-        systemctl stop firewalld.service
-        systemctl disable firewalld.service
-        systemctl enable iptables.service
-        systemctl enable ip6tables.service
+        systemctl stop firewalld.service 2>&1 | tee -a $LOG
+        systemctl disable firewalld.service 2>&1 | tee -a $LOG
+        systemctl enable iptables.service 2>&1 | tee -a $LOG
+        systemctl enable ip6tables.service 2>&1 | tee -a $LOG
 
-        /etc/sysconfig/iptables-config
+        /etc/sysconfig/iptables-config 2>&1 | tee -a $LOG
         # we're going to reload when the network is configured.
         # Lets not do this now.
         #systemctl restart iptables.service
 
         # TODO figure out the bestway to handle ipv6   	
-        systemctl restart ip6tables.service
+        systemctl restart ip6tables.service 2>&1 | tee -a $LOG
         set -e
         ;;
 	"no")
