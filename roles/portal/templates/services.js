2b4445cd0ca240c2a22af69042e82ad8b77c51f8
Miguel Gonzalez <migonzalvar@gmail.com>
2013-10-18 12:25:58 +0100

    portal: adapt to new JSON format

--- roles/portal/templates/services.js
+++ roles/portal/templates/services.js
@@ -14,8 +14,14 @@ function loadJSONservices(path, callback) {
 function loadServices(){
     loadJSONservices('services.json', function(servicelinks){
         var serviceList ="";
-        for(i=0;i<servicelinks.length;i++){
-            serviceList += "<div class='service'>"+"<a href="+servicelinks[i].link+">"+servicelinks[i].name+"</a>"+"<div id='desc'>"+servicelinks[i].description+"</div>"+"</div>";
+        for(var service in servicelinks) {
+            srv = servicelinks[service]
+            port = srv.port ? ":"+srv.port : ""
+            url = "//" + location.hostname + port + srv.path
+            serviceList += "<div class='service'>"
+            serviceList += "<a href='" + url + "'>" + srv.name + "</a>"
+            serviceList += "<div class='desc'>" + srv.description + "</div>"
+            serviceList += "</div>"
         }
         document.getElementById("servicelist").innerHTML=serviceList
     });
