3b1a09516d1fea763ddca5af817d76df3dfbb877
Tim Moody <tim@timmoody.com>
2013-10-23 14:53:23 -0400

    comments didn't work

--- roles/portal/files/xs-portal/index.en.php
+++ roles/portal/files/xs-portal/index.en.php
@@ -18,8 +18,9 @@
 
 <?php service_link("pathagar", "Read Books on Pathagar", "Pathagar is a Book Server that contains a local collection of books that you can read without downloading them from the internet."); ?>
 <?php // iiab_link("Internet In A Box", 
-                "Internet-in-a-Box is a copy of some of the most important material on the internet, such as the Wikipedia, stored locally where you can reach it easily.",
-                "Searching for Internet In A Box"); ?>
+//                "Internet-in-a-Box is a copy of some of the most important material on the internet, such as the Wikipedia, stored locally where you can reach it easily.",
+//                "Searching for Internet In A Box"); 
+?>
 <?php service_link("iiab", "Internet In A Box", "Internet-in-a-Box is a copy of some of the most important material on the internet, such as the Wikipedia, stored locally where you can reach it easily."); ?>
 <?php service_link("moodle", "Moodle Home Page", "Moodle is a collection of lessons and study materials organized to help you learn many fascinating things."); ?>
 <?php service_link("upload", "Upload Files", "From this link you can upload various files such as images and activities"); ?>
