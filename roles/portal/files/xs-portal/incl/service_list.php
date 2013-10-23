232b92ef1e24baccf48433b9f638e4e532b8ff73
Tim Moody <tim@timmoody.com>
2013-10-23 13:04:20 -0400

    read installed services from /etc/sysconfig/servicelist

--- roles/portal/files/xs-portal/incl/service_list.php
+++ roles/portal/files/xs-portal/incl/service_list.php
@@ -1,28 +1,16 @@
 <?php
-$serv_loc = array(    
-"activity-server" => "/activities",
-"iiab" => "/iiab/redirect",
-"moodle-xs" => "/moodle",
-"pathagar" => "/books/latest",
-"upload" => "/upload_activity.php"
-);
 
 $serv_arr  = array();
-$temp =  shell_exec('ls /etc/sysconfig/olpc-scripts/setup.d/installed');
-$tok = strtok($temp, " \n\t\r");
+$serv_arr = parse_ini_file ( '/etc/sysconfig/servicelist', true);
 
-while ($tok !== false) {
-//    echo "Word=$tok<br />";
-    $serv_arr[$tok] = 1;
-    $tok = strtok(" \n\t\r");
-}
+var_dump ($serv_arr);
 
 function service_link($service_key, $service_link_text, $service_link_desc)
 {
-	global $serv_arr, $serv_loc;
+	global $serv_arr;
 
   if (array_key_exists ( $service_key , $serv_arr )) {
-    $link_clause = '<div class="xsServiceWrapper"><div class="xsServiceLink"><a href="' . $serv_loc[$service_key] . '">' . $service_link_text . '</a></div>';
+    $link_clause = '<div class="xsServiceWrapper"><div class="xsServiceLink"><a href="' . $serv_arr[$service_key]["path"] . '">' . $service_link_text . '</a></div>';
     $link_desc = '<div class="xsServiceDesc">' . $service_link_desc . '</div></div><div style="clear:both"></div>';
     echo $link_clause;
     echo $link_desc;
@@ -31,7 +19,7 @@ function service_link($service_key, $service_link_text, $service_link_desc)
 
 function iiab_link($service_link_text, $service_link_desc, $service_search_text)
 {
-	global $serv_arr, $serv_loc;
+	global $serv_arr;
 
   if (array_key_exists ( "iiab" , $serv_arr )) {
   	// 1st show a text to indicate we are searching for content	  	
@@ -40,7 +28,7 @@ function iiab_link($service_link_text, $service_link_desc, $service_search_text)
     echo $link_clause;
     echo $search_text;
     // now the actual link
-    $link_clause = '<div id="iiab-link" style="display: none" class="xsServiceWrapper"><div class="xsServiceLink"><a href="' . $serv_loc["iiab"] . '">' . $service_link_text . '</a></div>';
+    $link_clause = '<div id="iiab-link" style="display: none" class="xsServiceWrapper"><div class="xsServiceLink"><a href="' . $serv_arr["iiab"]["path"] . '">' . $service_link_text . '</a></div>';
     $link_desc = '<div class="xsServiceDesc">' . $service_link_desc . '</div></div><div style="clear:both"></div>';    
     echo $link_clause;
     echo $link_desc;
