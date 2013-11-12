8ba14e4c7e4f04addcdfec777271d4bc7dc423aa
Miguel Gonzalez <migonzalvar@activitycentral.com>
2013-11-12 16:53:37 +0100

    pathagar: extract some values to vars file

--- roles/pathagar/templates/prod_settings.py
+++ roles/pathagar/templates/prod_settings.py
@@ -5,9 +5,9 @@ SECRET_KEY = '7ks@b7+gi^c4adff)6ka228#rd4f62v*g_dtmo*@i62k)qn=cs'
 DATABASES = {
     'default': {
             'ENGINE':'django.db.backends.postgresql_psycopg2',
-            'NAME': 'pathagar',
-            'USER': 'pathagar',
-            'PASSWORD': 'pathagar',
+            'NAME': '{{ pathagar_db_name }}',
+            'USER': '{{ pathagar_db_user }}',
+            'PASSWORD': '{{ pathagar_db_password }}',
             'HOST': '127.0.0.1',
             'PORT': '5432',
         }
