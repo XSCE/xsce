206db5303fcd34b94c5c24c3d34e145a2855a8a0
Miguel Gonzalez <migonzalvar@activitycentral.com>
2013-11-14 18:08:16 +0100

    pathagar: fix settings in wsgi.py

--- roles/pathagar/templates/wsgi.py
+++ roles/pathagar/templates/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pathagar.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pathagar.prod_settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
