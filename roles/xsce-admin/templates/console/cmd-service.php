934e953faab2753ef04d88ad97bf5a3ff3572080
Tim Moody <tim@timmoody.com>
2016-06-06 16:04:12 -0400

    change cmdsrv clients to use polling so can timeout

--- roles/xsce-admin/templates/console/cmd-service.php
+++ roles/xsce-admin/templates/console/cmd-service.php
@@ -5,14 +5,45 @@
 *  Sends command, expects response json back
 */
 
-//  Socket to talk to server
+$request_timeout = 10000; //  REQUEST_TIMEOUT in msecs, (> 1000!)
+
 $command = $_POST['command'];
-//echo "Command: $command <BR>";
-$context = new ZMQContext();
-$requester = new ZMQSocket($context, ZMQ::SOCKET_DEALER);
-$requester->connect("ipc:///run/cmdsrv_sock");
-$requester->send($command);
-$reply = $requester->recv();
+//$command = "TEST";
+// echo "Command: $command <BR>";
+
+$alert_param = ',"Alert": "True"';
+
+$read = $write = array();
+
+// See if XSCE-CMDSRV is running
+
+if (file_exists("/var/run/xsce-cmdsrv.pid")) {
+  try {
+    $context = new ZMQContext();
+    $requester = new ZMQSocket($context, ZMQ::SOCKET_DEALER);
+
+    //  Socket to talk to server
+    $requester->connect("ipc:///run/cmdsrv_sock");
+    $requester->setSockOpt(ZMQ::SOCKOPT_LINGER, 0);
+
+    $requester->send($command);
+
+    $poll = new ZMQPoll();
+    $poll->add($requester, ZMQ::POLL_IN);
+    $events = $poll->poll($read, $write, $request_timeout);
+    if ($events > 0) {
+      $reply = $requester->recv();
+    } else {
+    	$reply = '{"Error": "No Response from XSCE-CMDSRV in ' . $request_timeout . ' milliseconds"' . $alert_param . '}';
+    }
+  } catch (Exception $e) {
+    $reply = '{"Error": "' . $e->getMessage() . '"' . $alert_param . '}';
+  }
+
+} else {
+    $reply = '{"Error": "XSCE-CMDSRV is not running."' . $alert_param . '}';
+}
+
 header('Content-type: application/json');
 echo $reply;
 ?>
