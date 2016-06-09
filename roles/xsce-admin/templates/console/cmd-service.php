84f1fac7f1dc78bf2303f767f91c754e302d592c
Tim Moody <tim@timmoody.com>
2016-06-09 10:11:59 -0400

    add ready flag when server ready for commands

--- roles/xsce-admin/templates/console/cmd-service.php
+++ roles/xsce-admin/templates/console/cmd-service.php
@@ -18,29 +18,33 @@ $read = $write = array();
 // See if XSCE-CMDSRV is running
 
 if (file_exists("/var/run/xsce-cmdsrv.pid")) {
-  try {
-    $context = new ZMQContext();
-    $requester = new ZMQSocket($context, ZMQ::SOCKET_DEALER);
+	if (file_exists("/var/run/xsce-cmdsrv-ready")) {
+    try {
+      $context = new ZMQContext();
+      $requester = new ZMQSocket($context, ZMQ::SOCKET_DEALER);
 
-    //  Socket to talk to server
-    $requester->connect("ipc:///run/cmdsrv_sock");
-    $requester->setSockOpt(ZMQ::SOCKOPT_LINGER, 0);
+      //  Socket to talk to server
+      $requester->connect("ipc:///run/cmdsrv_sock");
+      $requester->setSockOpt(ZMQ::SOCKOPT_LINGER, 0);
 
-    $requester->send($command);
+      $requester->send($command);
 
-    $poll = new ZMQPoll();
-    $poll->add($requester, ZMQ::POLL_IN);
-    $events = $poll->poll($read, $write, $request_timeout);
-    if ($events > 0) {
-      $reply = $requester->recv();
-      if (strpos($reply, '"Error":') === false) {
-        $reply = '{"Data": ' . $reply;
+      $poll = new ZMQPoll();
+      $poll->add($requester, ZMQ::POLL_IN);
+      $events = $poll->poll($read, $write, $request_timeout);
+      if ($events > 0) {
+        $reply = $requester->recv();
+        if (strpos($reply, '"Error":') === false) {
+          $reply = '{"Data": ' . $reply;
+        }
+      } else {
+      	$reply = '{"Error": "No Response from XSCE-CMDSRV in ' . $request_timeout . ' milliseconds"' . $alert_param;
       }
-    } else {
-    	$reply = '{"Error": "No Response from XSCE-CMDSRV in ' . $request_timeout . ' milliseconds"' . $alert_param;
+    } catch (Exception $e) {
+      $reply = '{"Error": "' . $e->getMessage() . '"' . $alert_param;
     }
-  } catch (Exception $e) {
-    $reply = '{"Error": "' . $e->getMessage() . '"' . $alert_param;
+  } else {
+  	$reply = '{"Error": "XSCE-CMDSRV has started but is not ready."' . $alert_param;
   }
 
 } else {
