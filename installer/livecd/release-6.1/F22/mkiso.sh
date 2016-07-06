d5eca768b9378dfb14292262b300ff4e02cf44b6
Jerry Vonau <jvonau3@gmail.com>
2016-07-06 10:51:11 -0500

    mkiso - keep cache apart

--- installer/livecd/release-6.1/F22/mkiso.sh
+++ installer/livecd/release-6.1/F22/mkiso.sh
@@ -5,7 +5,7 @@ yum install livecd-tools
 KSCFG=ks.cfg
 BASE_ON=
 FSLABEL=F22-XSCE-6.1-LIVE
-CACHE=/opt/schoolserver/yum-packages
+CACHE=/opt/schoolserver/yum-packages-F22
 LOG=xsce-spin.log
 
 livecd-creator -c $KSCFG -f 6.1-b$FSLABEL --title $FSLABEL --product=Fedora --releasever=22 --cache=$CACHE 
