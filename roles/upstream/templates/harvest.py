e5a4e4c10fd0251f43886c1783e484a02dd9ed5b
George Hunt <georgejhunt@gmail.com>
2016-07-08 12:56:50 -0700

    move password to a file -- make it readable by apache, but not world

--- roles/upstream/templates/harvest.py
+++ roles/upstream/templates/harvest.py
@@ -8,11 +8,14 @@ import zipfile
 import json
 from time import sleep
 
+# go get the password for interaction with xscenet@gmail.com
+with open("/root/.xscenet_gmail",'r') as passwd:
+    credential = passwd.read()
 upenv = "{{ content_base }}/upstream"
 zips_dir = os.path.join(upenv,"html","zips")
 raw_dir = os.path.join(upenv,"html","raw_data")
 m = imaplib.IMAP4_SSL('imap.gmail.com')
-m.login('xscenet@gmail.com', 'immi96?Bronx')
+m.login('xscenet@gmail.com', credential)
 # declare location of dictionry with all our download data 
 #    -- the key for dictionary is datetime+download_url
 download_data = os.path.join(upenv,"downloads.json")
