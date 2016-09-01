c04c0170557e77ecb6e82136313e9549daab1595
George Hunt <georgejhunt@gmail.com>
2016-09-01 14:45:25 -0400

    delete iptables rules by linenumber, full opacity on button

--- roles/xsce-admin/files/cmdsrv/scripts/secure-desktop.sh
+++ roles/xsce-admin/files/cmdsrv/scripts/secure-desktop.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # close down the vnc remote desktop
 
+# delete any rules permitting 6080
+rules=`iptables -L INPUT --line-numbers |grep 6080|cut -d" " -f1`
+for rulenum in rules; do
+  iptables -D $rulenum
+done
+
 /etc/init.d/vnc stop
 systemctl stop websockify.service
-iptables -D INPUT -p tcp --dport 6080 -j ACCEPT
