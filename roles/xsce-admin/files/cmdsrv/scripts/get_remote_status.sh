5416115cc7517bfb2237a9fd505697c2c9fc617a
George Hunt <georgejhunt@gmail.com>
2016-09-25 19:25:35 -0400

    fixes to status

--- roles/xsce-admin/files/cmdsrv/scripts/get_remote_status.sh
+++ roles/xsce-admin/files/cmdsrv/scripts/get_remote_status.sh
@@ -3,14 +3,14 @@
 if [ ! -f /etc/xsce/xsce.env ]; then
   exit 1 # this file should exit, do not proceed
 fi
-remote_admin_allowed=`cat /etc/xsce/xsce.env|grep remote_admin_allowed`
+remote_admin_allowed=`cat /etc/xsce/xsce.env|grep remote_admin_allowed|cut -d"=" -f2`
 if [ $? -ne 0 ]; then
   echo "remote_admin_allowed=false" >> /etc/xsce/xsce.env
-  remote_admin-allowed='false'
+  remote_admin_allowed='false'
 fi
 if [ ! -f /etc/xsce/config_vars.yml ]; then
   exit 1 # this file should exit, do not proceed
 fi
 openvpn_enabled=`cat /etc/xsce/config_vars.yml|grep openvpn_enabled|cut -d":" -f2`
 teamviewer_enabled=`cat /etc/xsce/config_vars.yml|grep teamviewer_enabled|cut -d":" -f2`
-echo \{\"openvpn_enabled\":\"$openvpn_enabled\",\"teamviewer_allowed\":\"$teamviewer_enabled\",\"remote_admin_allowed\":$\"$remote_admin_allowed\"\}
+echo \{\"openvpn_enabled\":\"$openvpn_enabled\",\"teamviewer_enabed\":\"$teamviewer_enabled\",\"remote_admin_allowed\":\"$remote_admin_allowed\"\}
