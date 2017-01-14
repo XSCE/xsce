4f9e5dac2952f46a6204431efd88f9f5c5fddbba
George Hunt <georgejhunt@gmail.com>
2017-01-14 18:51:39 +0000

    fix [ -> ] in poweroff

--- roles/xsce-admin/files/cmdsrv/scripts/poweroffX.sh
+++ roles/xsce-admin/files/cmdsrv/scripts/poweroffX.sh
@@ -2,7 +2,7 @@
 
 . /etc/xsce/xsce.env
 
-if [ "$OS" = "Debian" [ || [ "$OS" = "raspbian" ]; then
+if [ "$OS" = "Debian" ] || [ "$OS" = "raspbian" ]; then
   /bin/sleep 3
   /sbin/shutdown -P now
 else
