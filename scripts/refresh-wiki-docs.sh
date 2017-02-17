88201ed61452b3ec8956280c50b073e53f9f908b
George Hunt <georgejhunt@gmail.com>
2017-02-17 00:02:37 -0500

    add in the stash

--- scripts/refresh-wiki-docs.sh
+++ scripts/refresh-wiki-docs.sh
@@ -1,18 +1,37 @@
-#!/bin/bash 
+#!/bin/bash -x
 # pull down repo wiki, and imbed in docs subdirectory
 
-WIKI=xsce.wiki
-REPO=https://github.com/xsce/${WIKI}.git
+source /etc/xsce/xsce.env
+REPONAME=xsce
+REPO=https://github.com/XSCE
+WIKI=xsce-wiki
 
 # this script is located in the scritps/ direcctory in the local repo
 SCRIPTDIR=$(dirname $0)
 
 if [ ! -d /tmp/${WIKI} ]; then
    mkdir -p /tmp/${WIKI}
-   git clone $REPO /tmp/${WIKI}
+   git clone $REPO/$REPONAME.wiki.git /tmp/${WIKI}
 fi
 
 echo Files available for update:
 ls -l /tmp/$WIKI
 
 rsync -v /tmp/$WIKI/* $SCRIPTDIR/../docs
+
+# convert the markdown docs to html
+which pandoc
+if [ $? -ne 0 ]; then
+   if [ "$OS" = "centos" ] || [ "$OS" = "fedora" ]; then
+      yum install -y pandoc
+   else
+      apt-get install -y pandoc
+   fi
+fi
+mkdir -p $SCRIPTDIR/../docs/html
+for f in `ls $SCRIPTDIR/../docs`; do
+    FTRIMMED=${f%.md}
+    pandoc -s $SCRIPTDIR/../docs/$f -o $SCRIPTDIR/../docs/html/$FTRIMMED.html
+    # make links refer to local directory
+    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)>|./\1.html)|" $SCRIPTDIR/../docs/html/$FTRIMMED.html
+done
