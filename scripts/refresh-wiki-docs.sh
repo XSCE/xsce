bb3856ec2707d67880afe0cad124cc86cd2a80b2
George Hunt <georgejhunt@gmail.com>
2017-02-17 19:07:06 -0500

    all the links in top level wiki work

--- scripts/refresh-wiki-docs.sh
+++ scripts/refresh-wiki-docs.sh
@@ -8,16 +8,14 @@ WIKI=xsce-wiki
 
 # this script is located in the scritps/ direcctory in the local repo
 SCRIPTDIR=$(dirname $0)
+pushd $SCRIPTDIR
 
 if [ ! -d /tmp/${WIKI} ]; then
    mkdir -p /tmp/${WIKI}
    git clone $REPO/$REPONAME.wiki.git /tmp/${WIKI}
 fi
 
-echo Files available for update:
-ls -l /tmp/$WIKI
-
-rsync -v /tmp/$WIKI/* $SCRIPTDIR/../docs
+rsync -v /tmp/$WIKI/* ../docs
 
 # convert the markdown docs to html
 which pandoc
@@ -28,10 +26,36 @@ if [ $? -ne 0 ]; then
       apt-get install -y pandoc
    fi
 fi
-mkdir -p $SCRIPTDIR/../docs/html
-for f in `ls $SCRIPTDIR/../docs`; do
+mkdir -p ../docs/html/online-help-offline
+#mkdir -p ../docs/html/offline-help
+
+for f in `ls ../docs`; do
     FTRIMMED=${f%.md}
-    pandoc -s $SCRIPTDIR/../docs/$f -o $SCRIPTDIR/../docs/html/$FTRIMMED.html
+    if [ $FTRIMMED = "Home" ]; then FTRIMMED=index;fi
+    pandoc -s ../docs/$f -o ../docs/html/online-help-offline/$FTRIMMED.html
     # make links refer to local directory
-    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|" $SCRIPTDIR/../docs/html/$FTRIMMED.html
+    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">|" ../docs/html/online-help-offline/$FTRIMMED.html
+    sed -i -e "s|schoolserver.org/faq|../FAQ|" ../docs/html/online-help-offline/$FTRIMMED.html
+    sed -i -e "s|$REPO/$REPONAME/blob/release-.*/\(.*\)\">|../\1.html\">|" ../docs/html/online-help-offline/$FTRIMMED.html
 done
+
+# copy the faq 
+wget -c http://wiki.laptop.org/go/XS_Community_Edition/FAQ -P ../docs/html
+
+# fetch the embedded help pages from the admin console
+#for f in `ls ../roles/xsce-admin/files/console/help`; do
+#    FTRIMMED=${f%.rst}
+#    pandoc -s ../roles/xsce-admin/files/console/help/$f -o ../docs/html/offline-help/$FTRIMMED.html
+#    # make links refer to local directory
+#    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|" ../docs/html/$FTRIMMED.html
+#done
+
+# fetch the recent release notes
+for f in `ls ../Release*`; do
+#    FTRIMMED=${f%.md}
+    FTRIMMED=${f:2}
+    pandoc -s $f -o ../docs/html/$FTRIMMED.html
+    # make links refer to local directory
+    sed -i -e "s|$REPO/$REPONAME/wiki/\(.*\)\">|./\1.html\">)|" ../docs/html/$FTRIMMED.html
+done
+popd
